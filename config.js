b4643197ee844a4b29e84e069a3630f3a3152e17
ericolimaeducador-ux <ericolima.educador@gmail.com>
2026-05-20 17:30:00 -0300

    Configure Supabase project

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.ABRACE_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://svbkrleuqtgzinloaquj.supabase.co",
+  supabaseAnonKey: "sb_publishable_K3L4cpCvqxV81sQO2ULPJg_iqbTE0Mg",
   pixKey: "03294572689",
   merchantName: "ABRACE DEUS",
   merchantCity: "SAO PAULO",
