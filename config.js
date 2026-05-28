e8f893ee0fb826b8809acc2eeedf805afd74832a
Erico Araujo <ericobrazil@hotmail.com>
2026-05-28 12:11:18 -0300

    Use Mercado Pago sandbox credentials

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.ABRACE_CONFIG = {
   supabaseUrl: "https://svbkrleuqtgzinloaquj.supabase.co",
   supabaseAnonKey: "sb_publishable_K3L4cpCvqxV81sQO2ULPJg_iqbTE0Mg",
   currency: "BRL",
-  mercadoPagoPublicKey: "APP_USR-b039404c-8df6-40c5-aaa5-b5f44b69fe59",
+  mercadoPagoPublicKey: "TEST-4b7569fa-ad1d-41a2-8ac5-5c2ef2393d8d",
   checkoutFunctionUrl: "https://svbkrleuqtgzinloaquj.supabase.co/functions/v1/checkout",
   shippingOriginZipCode: "00000000"
 };
